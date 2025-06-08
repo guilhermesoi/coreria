84ed92468a9bb724bce0866f722878ec99516914
guilhermesoi <guilhermeidhabbo@gmail.com>
2025-06-08 10:04:44 -0300

    Update server.js

--- server.js
+++ server.js
@@ -34,5 +34,5 @@ app.post('/chat', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ||;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
